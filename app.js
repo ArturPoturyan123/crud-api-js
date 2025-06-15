74cca91778078c43bd6e6513faee02883b8f58a1
arturp <poturyan@gmail.com>
2025-06-15 22:26:19 +0400

    added new render port

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database");
-    app.listen(3000, () => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
       console.log("Server is running on port 3000");
     });
   })
